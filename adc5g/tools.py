7062b32e4ab64ee66857998269aa609dffb6d9bd
Rurik A Primiani <rprimian@cfa.harvard.edu>
2011-10-05 16:47:09 -0400

    Modified the BRAM and CTRL formats to work with the more standard 'snapshot' block of the CASPER libraries.

--- adc5g/tools.py
+++ adc5g/tools.py
@@ -10,50 +10,28 @@ from numpy import (
 
 
 CTRL_FMT = "snap_{chan}_ctrl"
-BRAM_FMT = "snap_{chan}_bram_{lmsb}"
-
-
-def interleave(lsb, msb, bitwidth=8):
-    """
-    Re-interleaves the snapshots from the 32-bit LSB 
-    and MSB block memories to form the correctly
-    ordered original snapshot.
-    """
-
-    out = []
-    demux = int(32/bitwidth)
-
-    for i in range(0, len(lsb), demux):
-        out.extend(msb[i:i+demux])
-        out.extend(lsb[i:i+demux])
-
-    return array(out)
+BRAM_FMT = "snap_{chan}_bram"
 
 
 def get_snapshot(roach, chan, length=2**14, 
                  bitwidth=8, ctrl_fmt=CTRL_FMT, bram_fmt=BRAM_FMT, 
                  force=True):
     """
-    Reads a one-channel, 64-bit snapshot off the given 
+    Reads a one-channel snapshot off the given 
     ROACH and returns the time-ordered samples.
     """
 
-    roach.write_int(ctrl_fmt.format(chan=chan), 0)
-    roach.write_int(ctrl_fmt.format(chan=chan), 1)
+    roach.write_int(ctrl_fmt.format(chan=chan), 2)
+    roach.write_int(ctrl_fmt.format(chan=chan), 3)
 
     if force:
         roach.write_int('force_capture', 0)
         roach.write_int('force_capture', 1)
+        
+    bram_name = bram_fmt.format(chan=chan)
+    data = unpack('%iB' %length, roach.read(bram_name, length))
 
-    lsb_bram_name = bram_fmt.format(chan=chan, lmsb='lsb')
-    lsb_raw = roach.read(lsb_bram_name, length)
-    lsb = unpack('%iB'%length, lsb_raw)
-
-    msb_bram_name = bram_fmt.format(chan=chan, lmsb='msb')
-    msb_raw = roach.read(msb_bram_name, length)
-    msb = unpack('%iB'%length, msb_raw)
-
-    return interleave(lsb, msb, bitwidth)
+    return array(data)
 
 
 def get_psd(roach, chan, samp_freq, length=2**14,
