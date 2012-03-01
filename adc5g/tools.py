5e0e0eec17e23681fea8e38ee6dc045dceeb4f2e
Rurik A Primiani <rprimian@cfa.harvard.edu>
2012-03-01 14:52:48 -0500

    Added functions to read ADC5G's control, offset, gain, and phase registers over SPI.
    The functions are adc5g.tools.get_spi_* and attempt to handle the conversion
    between real values and register values. Because the offset, gain, and phase
    registers have limited resolution, if you write a certain value you are not
    necessarily guaranteed to get that exact value on a read.

--- adc5g/tools.py
+++ adc5g/tools.py
@@ -27,6 +27,21 @@ def set_spi_register(roach, zdok_n, reg_addr, reg_val):
     roach.blindwrite(ADC_CONTROLLER, spi_data, offset=0x4+zdok_n*0x4)
 
 
+def get_spi_register(roach, zdok_n, reg_addr):
+    """
+    Gets the value of an ADC's register over SPI
+    """
+    spi_data = pack(SPI_DATA_FMT, 0x0, reg_addr, 0x01)
+    roach.blindwrite(ADC_CONTROLLER, spi_data, offset=0x4+zdok_n*0x4)
+    raw = roach.read(ADC_CONTROLLER, 0x4, offset=0x4+zdok_n*0x4)
+    print raw.encode('string_escape')
+    reg_val, old_reg_addr, config_done = unpack(SPI_DATA_FMT, raw)
+    if old_reg_addr is not reg_addr:
+        raise ValueError("Could not read SPI register!")
+    else:
+        return reg_val
+
+
 def set_spi_control(roach, zdok_n, adcmode=8, stdby=0, dmux=1, bg=1, bdw=3, fs=0, test=0):
     """
     Sets the control register of an ADC over SPI.
@@ -38,6 +53,24 @@ def set_spi_control(roach, zdok_n, adcmode=8, stdby=0, dmux=1, bg=1, bdw=3, fs=0
     """
     reg_val = adcmode + (stdby<<4) + (dmux<<6) + (bg<<7) + (bdw<<8) + (fs<<10) + (test<<12)
     set_spi_register(roach, zdok_n, CONTROL_REG_ADDR, reg_val)
+
+
+def get_spi_control(roach, zdok_n):
+    """
+    Gets the current value of the control register of an ADC over SPI.
+    
+    See: http://www.e2v.com/e2v/assets/File/documents/broadband-data-converters/doc0846I.pdf
+         specifically section 8.7.3 for more details and an explanation of returned values.
+    """
+    #reg_val = adcmode + (stdby<<4) + (dmux<<6) + (bg<<7) + (bdw<<8) + (fs<<10) + (test<<12)
+    reg_val = get_spi_register(roach, zdok_n, CONTROL_REG_ADDR-0x80)
+    return {'adcmode' : reg_val & 0xf,
+            'stdby'   : (reg_val>>4) & 0x3,
+            'dmux'    : (reg_val>>6) & 0x1,
+            'bg'      : (reg_val>>7) & 0x1,
+            'bdw'     : (reg_val>>8) & 0x3,
+            'fs'      : (reg_val>>10) & 0x1,
+            'test'    : (reg_val>>12) & 0x1}
 
 
 def set_spi_offset(roach, zdok_n, chan, offset):
@@ -55,6 +88,20 @@ def set_spi_offset(roach, zdok_n, chan, offset):
     set_spi_register(roach, zdok_n, CALCTRL_REG_ADDR, 2<<2)
 
 
+def get_spi_offset(roach, zdok_n, chan):
+    """
+    Sets the offset value of one of the four channels on an ADC over SPI.
+    
+    The offset is a float ranging from -50 mV to +50 mV (with a resolution of 0.4 mV).
+
+    See: http://www.e2v.com/e2v/assets/File/documents/broadband-data-converters/doc0846I.pdf
+         specifically section 8.7.14 for more details and possible values.
+    """
+    set_spi_register(roach, zdok_n, CHANSEL_REG_ADDR, chan)
+    reg_val = get_spi_register(roach, zdok_n, EXTOFFS_REG_ADDR-0x80)
+    return (reg_val - 0x80)*(100./255)
+
+
 def set_spi_gain(roach, zdok_n, chan, gain):
     """
     Sets the gain value of one of the four channels on an ADC over SPI.
@@ -70,6 +117,20 @@ def set_spi_gain(roach, zdok_n, chan, gain):
     set_spi_register(roach, zdok_n, CALCTRL_REG_ADDR, 2<<4)
 
 
+def get_spi_gain(roach, zdok_n, chan):
+    """
+    Gets the gain value of one of the four channels on an ADC over SPI.
+    
+    The gain is a float ranging from -18% to +18% (with a resolution of 0.14%).
+
+    See: http://www.e2v.com/e2v/assets/File/documents/broadband-data-converters/doc0846I.pdf
+         specifically section 8.7.16 for more details and possible values.
+    """
+    set_spi_register(roach, zdok_n, CHANSEL_REG_ADDR, chan)
+    reg_val = get_spi_register(roach, zdok_n, EXTGAIN_REG_ADDR-0x80)
+    return (reg_val - 0x80)*(36./255)
+
+
 def set_spi_phase(roach, zdok_n, chan, phase):
     """
     Sets the phase value of one of the four channels on an ADC over SPI.
@@ -83,6 +144,20 @@ def set_spi_phase(roach, zdok_n, chan, phase):
     set_spi_register(roach, zdok_n, CHANSEL_REG_ADDR, chan)
     set_spi_register(roach, zdok_n, EXTPHAS_REG_ADDR, reg_val)
     set_spi_register(roach, zdok_n, CALCTRL_REG_ADDR, 2<<6)
+
+
+def get_spi_phase(roach, zdok_n, chan):
+    """
+    Gets the phase value of one of the four channels on an ADC over SPI.
+    
+    The phase is a float ranging from -14 ps to +14 ps (with a resolution of 110 fs).
+
+    See: http://www.e2v.com/e2v/assets/File/documents/broadband-data-converters/doc0846I.pdf
+         specifically section 8.7.18 for more details and possible values.
+    """
+    set_spi_register(roach, zdok_n, CHANSEL_REG_ADDR, chan)
+    reg_val = get_spi_register(roach, zdok_n, EXTPHAS_REG_ADDR-0x80)
+    return (reg_val - 0x80)*(28./255)
 
 
 def get_snapshot(roach, snap_name, bitwidth=8):
