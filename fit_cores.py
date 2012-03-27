0467affb3bac32abe65ca14a4f8f878ea2351633
Robert Wilson <rwilson@bobs-imac.home>
2012-03-27 16:37:38 -0400

    Added the computation of the average residual of the fit to each core at each code.  write the results in fname.res Add the ability to average the residua;s and off, gain and phase over multiple runs. Control printing and erasing the average values

--- fit_cores.py
+++ fit_cores.py
@@ -4,8 +4,14 @@
 import sys
 import os
 import math
-from scipy import *
+#from scipy import *
+from numpy import array, zeros, savetxt
 from scipy.optimize import leastsq
+
+avg_result = zeros((14), dtype=float)
+result_cnt = 0
+code_errors = zeros((256,4), dtype='float')
+ce_counts = zeros((256, 4), dtype='int32')
 
 def fitval(p, s, c):
   return p[0] +  p[1] * s + p[2] * c
@@ -14,7 +20,16 @@ def fitval(p, s, c):
 def residuals(p, s, c, adc):
   return adc - fitval(p, s, c)
 
-def fitc(sig_freq, samp_freq, df_name):
+def fitc(sig_freq, samp_freq, df_name, clear_avgs=True, prnt=True):
+  """
+  Given a file containing a snapshot of data, separate the data from the
+  4 cores and fit a separate sine wave to each.  From the dc offset, gain
+  and phase of the four fits report the average and the difference of each
+  core from the average.  Write a line in fname.fit giving these values.
+  Compute the average difference between the fitted value and emasured value
+  for each level of each core averaged of=ver the samples.
+  """
+  global avg_result, result_cnt, code_errors, ce_counts
   p0 = [7.5, 5.0, 5.0]
   
   # Create lists to hold parameters at sample rate
@@ -25,18 +40,15 @@ def fitc(sig_freq, samp_freq, df_name):
   del_phi = 2 * math.pi * sig_freq / samp_freq
   ifd=open(df_name, 'r')
   ffd = open(df_name + ".fit", 'a')
-  outfile = df_name + ".c1"
-  ffd1 = open(outfile, 'w')
-#  print >>ffd, 'f1 = "%s"' % (outfile)
-  outfile = df_name + ".c2"
-  ffd2 = open(outfile, 'w')
-#  print >>ffd, 'f2 = "%s"' % (outfile)
-  outfile = df_name + ".c3"
-  ffd3 = open(outfile, 'w')
-#  print >>ffd, 'f3 = "%s"' % (outfile)
-  outfile = df_name + ".c4"
-  ffd4 = open(outfile, 'w')
-#  print >>ffd, 'f4 = "%s"' % (outfile)
+  if prnt:
+    outfile = df_name + ".c1"
+    ffd1 = open(outfile, 'w')
+    outfile = df_name + ".c2"
+    ffd2 = open(outfile, 'w')
+    outfile = df_name + ".c3"
+    ffd3 = open(outfile, 'w')
+    outfile = df_name + ".c4"
+    ffd4 = open(outfile, 'w')
   data_cnt = 0
   for line in ifd:
     if line[0] == "#":
@@ -76,7 +88,7 @@ def fitc(sig_freq, samp_freq, df_name):
   z1 = z_fact * plsq1[0][0]
   s1a = plsq1[0][1]
   c1a = plsq1[0][2]
-  amp1 = sqrt(s1a**2 + c1a**2)
+  amp1 = math.sqrt(s1a**2 + c1a**2)
   dly1 = d_fact*math.atan2(s1a, c1a)
   
   args2 = (array(s2), array(c2), array(core2))
@@ -84,7 +96,7 @@ def fitc(sig_freq, samp_freq, df_name):
   z2 = z_fact * plsq2[0][0]
   s2a = plsq2[0][1]
   c2a = plsq2[0][2]
-  amp2 = sqrt(s2a**2 + c2a**2)
+  amp2 = math.sqrt(s2a**2 + c2a**2)
   dly2 = d_fact*math.atan2(s2a, c2a)
   
   args3 = (array(s3), array(c3), array(core3))
@@ -92,7 +104,7 @@ def fitc(sig_freq, samp_freq, df_name):
   z3 = z_fact * plsq3[0][0]
   s3a = plsq3[0][1]
   c3a = plsq3[0][2]
-  amp3 = sqrt(s3a**2 + c3a**2)
+  amp3 = math.sqrt(s3a**2 + c3a**2)
   dly3 = d_fact*math.atan2(s3a, c3a)
   
   args4 = (array(s4), array(c4), array(core4))
@@ -100,7 +112,7 @@ def fitc(sig_freq, samp_freq, df_name):
   z4 = z_fact * plsq4[0][0]
   s4a = plsq4[0][1]
   c4a = plsq4[0][2]
-  amp4 = sqrt(s4a**2 + c4a**2)
+  amp4 = math.sqrt(s4a**2 + c4a**2)
   dly4 = d_fact*math.atan2(s4a, c4a)
   
   avz = (z1+z2+z3+z4)/4.0
@@ -110,25 +122,78 @@ def fitc(sig_freq, samp_freq, df_name):
   a3p = 100*(amp3 - avamp)/avamp
   a4p = 100*(amp4 - avamp)/avamp
   avdly = (dly1+dly2+dly3+dly4)/4.0
-  print "#%6.2f  zero(mV) amp(%%)  dly(ps)" % (sig_freq)
-  print "#avg    %7.4f %7.4f %8.4f" %  (avz, avamp, avdly)
-  print "core A  %7.4f %7.4f %8.4f" %  (z1-avz, a1p, dly1-avdly)
-  print "core B  %7.4f %7.4f %8.4f" %  (z3-avz, a3p, dly3-avdly)
-  print "core C  %7.4f %7.4f %8.4f" %  (z2-avz, a2p, dly2-avdly)
-  print "core D  %7.4f %7.4f %8.4f" %  (z4-avz, a4p, dly4-avdly)
-  print
+  if prnt:
+    print "#%6.2f  zero(mV) amp(%%)  dly(ps) (adj by .4, .14, .11)" % (sig_freq)
+    print "#avg    %7.4f %7.4f %8.4f" %  (avz, avamp, avdly)
+    print "core A  %7.4f %7.4f %8.4f" %  (z1-avz, a1p, dly1-avdly)
+    print "core B  %7.4f %7.4f %8.4f" %  (z3-avz, a3p, dly3-avdly)
+    print "core C  %7.4f %7.4f %8.4f" %  (z2-avz, a2p, dly2-avdly)
+    print "core D  %7.4f %7.4f %8.4f" %  (z4-avz, a4p, dly4-avdly)
+    print
 
-  print >> ffd, "%9.4f %9.4f %9.4f %7.4f %7.4f %8.4f %7.4f %7.4f %8.4f %7.4f %7.4f %8.4f %7.4f %7.4f %8.4f" % (sig_freq, avz, avamp, z1-avz, a1p, dly1-avdly, z3-avz, a3p, dly3-avdly, z2-avz, a2p, dly2-avdly, z4-avz, a4p, dly4-avdly)
-  
+  if clear_avgs:
+    avg_result = zeros((15), dtype=float)
+    result_cnt = 0
+    code_errors = zeros((256,4), dtype='float')
+    ce_counts = zeros((256, 4), dtype='int32')
+
+  result = (sig_freq, avz, avamp, \
+      z1-avz, a1p, dly1-avdly, z3-avz, a3p, dly3-avdly, \
+      z2-avz, a2p, dly2-avdly, z4-avz, a4p, dly4-avdly)
+  result_fmt = "%8.4f "*15
+#  if prnt:
+#    print >>ffd, result_fmt % result
+  avg_result += array(result)
+  result_cnt += 1
+#  print result_fmt % avg_resul
+#  aresult = array(result)
+  if prnt and result_cnt > 1:
+    t = tuple(avg_result/result_cnt)
+    print >>ffd, result_cnt,
+    print >>ffd,  result_fmt % t
+    print "average of %d measurements" % (result_cnt)
+    print "#avg    %7.4f %7.4f %8.4f" %  (t[1], t[2], 0)
+    print "core A  %7.4f %7.4f %8.4f" %  t[3:6]
+    print "core B  %7.4f %7.4f %8.4f" %  t[6:9]
+    print "core C  %7.4f %7.4f %8.4f" %  t[9:12]
+    print "core D  %7.4f %7.4f %8.4f" %  t[12:15]
+    print
+
+  # for each core (n), accumulate the sum of the residuals at each output code
+  # in code_errors[code][n]
+  # and the count of residuals added in ce_counts[code][n]
   Fit1 = fitval(plsq1[0], args1[0], args1[1])
   for i in range(data_cnt/4):
-    print >>ffd1, "%d %d %.2f" % (4 * i, core1[i], Fit1[i])
+    code = core1[i]
+    if prnt:
+      print >>ffd1, "%d %d %.2f" % (4 * i, code, Fit1[i])
+    code_errors[code][0] += code - Fit1[i]
+    ce_counts[code][0] += 1
   Fit2 = fitval(plsq2[0], args2[0], args2[1])
   for i in range(data_cnt/4):
-    print >>ffd3, "%d %d %.2f" % (4 * i + 1, core2[i], Fit2[i])
+    code = core2[i]
+    if prnt:
+      print >>ffd3, "%d %d %.2f" % (4 * i + 1, code, Fit2[i])
+    code_errors[code][1] += code - Fit2[i]
+    ce_counts[code][1] += 1
   Fit3 = fitval(plsq3[0], args3[0], args3[1])
   for i in range(data_cnt/4):
-    print >>ffd2, "%d %d %.2f" % (4 * i + 2, core3[i], Fit3[i])
+    code = core3[i]
+    if prnt:
+      print >>ffd2, "%d %d %.2f" % (4 * i + 2, code, Fit3[i])
+    code_errors[code][2] += code - Fit3[i]
+    ce_counts[code][2] += 1
   Fit4 = fitval(plsq4[0], args4[0], args4[1])
   for i in range(data_cnt/4):
-    print >>ffd4, "%d %d %.2f" % (4 * i + 3, core4[i], Fit4[i])
+    code = core4[i]
+    if prnt:
+      print >>ffd4, "%d %d %.2f" % (4 * i + 3, code, Fit4[i])
+    code_errors[code][3] += code - Fit4[i]
+    ce_counts[code][3] += 1
+  rfd = open(df_name + '.res', "w")
+  if prnt:
+    for code in range(256):
+      if ce_counts[code].min() > 4:
+        e = code_errors[code]/ce_counts[code]
+        print >>rfd, "%3d %5.3f %5.3f %5.3f %5.3f" % \
+	    (code, e[0], e[1], e[2], e[3])
