1b02b45e9f30259bfaf54cfac19c9977609772b1
Rurik A Primiani <rprimian@cfa.harvard.edu>
2012-07-17 14:03:58 -0400

    Added a Python unittest framework.
    Also added some basic connectivity tests along with it as well as
    some option parsing to the 'test_adc5g' script. To see the avail-
    able options just run it with the -h flag.

--- test_adc5g.py
+++ test_adc5g.py
@@ -1,7 +1,58 @@
+import unittest
+from optparse import OptionParser
+
 import adc5g
+try:
+    from corr import katcp_wrapper
+    REMOTE_POSSIBLE = True
+except ImportError:
+    REMOTE_POSSIBLE = False
 
-roach = adc5g.LocalRoachClient()
-print "Estimated FPGA clock rate: %.2f MHz" % round(roach.est_brd_clk(), 2)
 
-print "Devices found:"
-print '\n'.join(roach.listdev())
+class TestBasics(unittest.TestCase):
+    
+    @classmethod
+    def setUpClass(cls):
+        global roach
+        cls.roach = roach
+
+    def test_connected(self):
+        "testing katcp connectivity"
+        self.assertTrue(self.roach.is_connected())
+
+    def test_ping(self):
+        "testing katcp pingability"
+        self.assertTrue(self.roach.ping())
+
+    
+def run_tests(verbosity):
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestBasics)
+    unittest.TextTestRunner(verbosity=verbosity).run(suite)
+
+
+def main():
+    global roach
+    parser = OptionParser()
+    parser.add_option("-v", action="store_true", dest="verbose",
+                      help="use verbose output while testing")
+    parser.add_option("-r", "--remote",
+                      dest="remote", metavar="HOST:PORT",
+                      help="run tests remotely over katcp using HOST and PORT")
+    (options, args) = parser.parse_args()
+    if options.verbose:
+        verbosity = 2
+    else:
+        verbosity = 1
+    if options.remote:
+        if REMOTE_POSSIBLE:
+            roach = katcp_wrapper.FpgaClient(*options.remote.split(':'))
+        else:
+            raise ImportError("corr package was not found, "
+                              "remote operation not possible!")
+    else:
+        roach = adc5g.LocalRoachClient()
+    run_tests(verbosity)
+
+
+if __name__ == "__main__":
+    main()
