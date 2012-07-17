f6e3ca1fae0cd82d72f14a16584f6f1d851b4dbf
Rurik A Primiani <rprimian@cfa.harvard.edu>
2012-07-17 16:27:56 -0400

    Added a boffile option and a test case that makes sure it's available.
    If the requested boffile is not available on the ROACH an error is given.

--- test_adc5g.py
+++ test_adc5g.py
@@ -9,20 +9,33 @@ except ImportError:
     REMOTE_POSSIBLE = False
 
 
+class BofList(list):
+
+    def __repr__(self):
+        size = self.__len__()
+        return "%d available BOF files" % size
+
+
 class TestBasics(unittest.TestCase):
     
     def setUp(self):
         if not hasattr(self, '_roach'):
-            global roach
+            global roach, boffile
             self._roach = roach
+            self._dut = boffile
 
     def test_connected(self):
-        "testing katcp connectivity"
+        "testing roach connectivity"
         self.assertTrue(self._roach.is_connected())
 
     def test_ping(self):
-        "testing katcp pingability"
+        "testing roach pingability"
         self.assertTrue(self._roach.ping())
+
+    def test_listbof(self):
+        "checking if requested bof is available"
+        bofs = BofList(self._roach.listbof())
+        self.assertIn(self._dut, bofs)
 
     
 def run_tests(verbosity):
@@ -31,13 +44,16 @@ def run_tests(verbosity):
 
 
 def main():
-    global roach
+    global roach, boffile
     parser = OptionParser()
     parser.add_option("-v", action="store_true", dest="verbose",
                       help="use verbose output while testing")
     parser.add_option("-r", "--remote",
                       dest="remote", metavar="HOST:PORT",
                       help="run tests remotely over katcp using HOST and PORT")
+    parser.add_option("-b", "--boffile",
+                      dest="boffile", metavar="BOFFILE", default="adc5g_r2_snap.bof",
+                      help="test using the BOFFILE bitcode")
     (options, args) = parser.parse_args()
     if options.verbose:
         verbosity = 2
@@ -52,6 +68,7 @@ def main():
                               "remote operation not possible!")
     else:
         roach = adc5g.LocalRoachClient()
+    boffile = options.boffile
     run_tests(verbosity)
 
 
